865f538a403c67d6c3bdcef2e8d3a24e0c819084
Alialisk <99403653+Aliasik@users.noreply.github.com>
2024-03-29 07:55:18 +0100

    Class Funcitons over here!

---
+++ features/common.js
@@ -0,0 +1,67 @@
+export class Functions {
+  randomNumber(countOfNumbers) {
+    return Math.floor(Math.random() * countOfNumbers);
+  }
+  compareTiles(mapTile, structureTile) {
+    return mapTile.tileX === structureTile.tileX && mapTile.tileY === structureTile.tileY;
+  }
+  findObjFromArrByCoordinates(map, x, y) {
+    // you must remember coordinates are always must be like tileX and tileY keys
+    const obj = map.find((obj) => obj.tileX === x && obj.tileY === y);
+    const index = map.findIndex((obj) => obj.tileX === x && obj.tileY === y);
+    return {
+      ...obj,
+      index,
+    };
+  }
+  randomCoordinatesForRoads(xy, count) {
+    // xy here is vector (x or y)
+    console.log(count);
+    let arr = [];
+    for (let index = 0; index < count; index++) {
+      let value;
+      if (xy == 'x') {
+        do {
+          value = this.randomNumber(100);
+        } while (value > this.maxX || arr.includes(value));
+      }
+      if (xy == 'y') {
+        do {
+          value = this.randomNumber(100);
+        } while (value > this.maxY || arr.includes(value));
+      }
+
+      arr.push(value);
+    }
+    return arr;
+  }
+  randomCoordinatesOnEmptyTile(map, count, tileType, maxX, maxY) {
+    // this func used to return tile with {tileType: 'tile'}
+    // because stuff can spawn only on free tile
+    console.log('map', count, tileType, maxX, maxY);
+    let coordinates = [];
+    for (let index = 0; index < count; index++) {
+      let x_coordinate = this.randomNumber(100);
+      let y_coordinate = this.randomNumber(100);
+
+      do {
+        x_coordinate = this.randomNumber(100);
+      } while (x_coordinate > maxX);
+      do {
+        y_coordinate = this.randomNumber(100);
+      } while (y_coordinate > maxY);
+
+      // it includes index of coordinate!!
+      const comparable = this.findObjFromArrByCoordinates(map, x_coordinate, y_coordinate);
+
+      if (comparable.tileType === 'tile') {
+        coordinates.push({ ...comparable, tileType });
+      } else {
+        // extra iteration if its not empty tile
+        index--;
+      }
+    }
+
+    return coordinates;
+  }
+}
