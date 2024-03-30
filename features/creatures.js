e35854cc07a178cf0baac1f1a6e102cb4b0a8a42
Alialisk <99403653+Aliasik@users.noreply.github.com>
2024-03-30 14:16:02 +0100

    Implemented: character moving on map. Must upd to interact with map!!

--- features/creatures.js
+++ features/creatures.js
@@ -1,8 +1,12 @@
 import { Game } from '../index.js';
+import { Functions } from './common.js';
 
-class Entity extends Game {
+class Entity extends Functions {
   constructor() {
     super();
+    // name must be a uid of living entities
+    this.name = null;
+    this.tileType = null;
     this.hp = 10;
     this.atc = 1;
     this.coordinates = {
@@ -11,17 +15,17 @@ class Entity extends Game {
     };
   }
 
-  spawnEntity(entity, tileType) {
+  spawnEntity(entityInfo, tileType) {
     let field = document.querySelector('.field');
     let charElem = document.createElement('div');
 
     // choose tyleType
     charElem.className = tileType;
     // UI position
-    charElem.style = `left: ${entity.tileX * 50}px; top: ${entity.tileY * 50}px;`;
+    charElem.style = `left: ${entityInfo.tileX * 50}px; top: ${entityInfo.tileY * 50}px;`;
     // scroll to main char
     if (tileType == 'tileP') {
-      this.autoScrollToEntity(field, entity, this.maxX, this.maxY);
+      this.autoScrollToEntity(field, entityInfo, entityInfo.maxX, entityInfo.maxY);
     }
 
     field.append(charElem);
@@ -32,20 +36,78 @@ export class Character extends Entity {
   constructor(gameInstance) {
     super();
     this.game = gameInstance;
+    this.entityInfo = null;
 
-    this.character = null;
+    this.name = 'Hero';
+    this.tileType = 'tileP';
   }
   init() {
-    console.log(this.game.map);
-
-    this.character = this.randomCoordinatesOnEmptyTile(
+    this.entityInfo = this.randomCoordinatesOnEmptyTile(
       this.game.map,
       1,
-      'tileP',
-      this.maxX,
-      this.maxY,
+      this.tileType,
+      this.game.maxX,
+      this.game.maxY,
     )[0];
+    this.entityInfo = { ...this.entityInfo, maxX: this.game.maxX, maxY: this.game.maxY };
 
-    this.spawnEntity(this.character, 'tileP');
+    this.coordinates = {
+      x: this.entityInfo.tileX,
+      y: this.entityInfo.tileY,
+    };
+    this.spawnEntity(this.entityInfo, this.tileType);
+
+    const self = this;
+    document.addEventListener('keydown', function (event) {
+      // Check if the pressed key is 'W', 'A', 'S', or 'D'
+      if (event.key === 'w' || event.key === 'W' || event.key === 'ц' || event.key === 'Ц') {
+        console.log('Key W pressed');
+        self.move('top');
+        // Your action for 'W' key press goes here
+      } else if (event.key === 'a' || event.key === 'A' || event.key === 'ф' || event.key === 'Ф') {
+        console.log('Key A pressed');
+        self.move('left');
+        // Your action for 'A' key press goes here
+      } else if (event.key === 's' || event.key === 'S' || event.key === 'ы' || event.key === 'Ы') {
+        console.log('Key S pressed');
+        self.move('bottom');
+        // Your action for 'S' key press goes here
+      } else if (event.key === 'd' || event.key === 'D' || event.key === 'в' || event.key === 'В') {
+        console.log('Key D pressed');
+        self.move('right');
+        // Your action for 'D' key press goes here
+      }
+    });
+  }
+
+  move(direction) {
+    if (direction == 'top') {
+      const entityElem = document.querySelector('.tileP');
+      this.coordinates.y--;
+      entityElem.style = `left: ${this.coordinates.x * 50}px; top: ${this.coordinates.y * 50}px;`;
+    }
+    if (direction == 'bottom') {
+      const entityElem = document.querySelector('.tileP');
+      this.coordinates.y++;
+      entityElem.style = `left: ${this.coordinates.x * 50}px; top: ${this.coordinates.y * 50}px;`;
+    }
+    if (direction == 'right') {
+      const entityElem = document.querySelector('.tileP');
+      this.coordinates.x++;
+      entityElem.style = `left: ${this.coordinates.x * 50}px; top: ${this.coordinates.y * 50}px;`;
+    }
+    if (direction == 'left') {
+      const entityElem = document.querySelector('.tileP');
+      this.coordinates.x--;
+      entityElem.style = `left: ${this.coordinates.x * 50}px; top: ${this.coordinates.y * 50}px;`;
+    }
+
+    const field = document.querySelector('.field');
+    this.autoScrollToEntity(
+      field,
+      { ...this.entityInfo, tileX: this.coordinates.x, tileY: this.coordinates.y },
+      this.entityInfo.maxX,
+      this.entityInfo.maxY,
+    );
   }
 }
