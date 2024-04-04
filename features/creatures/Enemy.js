63e5fbcc7cb77add9db1925fabba1dadda04a6e7
Alialisk <99403653+Aliasik@users.noreply.github.com>
2024-04-04 22:04:31 +0200

    Implemented Update: refactored npc movemenst and make them imposible to walk throw walls and entities

--- features/creatures/Enemy.js
+++ features/creatures/Enemy.js
@@ -10,7 +10,6 @@ export default class Enemy extends Entity {
     this.entityInfo = null;
   }
   init(id) {
-    console.log(this);
     // givin uid to enemy
     this.id = id;
     // gen start parameters to enemy
@@ -38,5 +37,106 @@ export default class Enemy extends Entity {
       y: this.entityInfo.tileY,
     };
     this.spawnEntity(this.entityInfo, this.game.entities);
+    this.moveLogic(id);
+  }
+
+  moveLogic(id) {
+    const recursy = () => {
+      const enemyEntity = this.findObjFromArrById(this.game.entities, id);
+      const hero = this.findObjFromArrById(this.game.entities, 'tileP');
+
+      // calculate distances between hero and enemy
+      let distaceX = hero.tileX - enemyEntity.tileX;
+      let distanceY = hero.tileY - enemyEntity.tileY;
+
+      // according absolutes of distace, execute .move()
+      if (Math.abs(distaceX) > Math.abs(distanceY)) {
+        if (distaceX > 0) {
+          // X
+          // npc going right way
+          const comparableEntities = this.findObjFromArrByCoordinates(
+            this.game.entities,
+            enemyEntity.tileX + 1,
+            enemyEntity.tileY,
+          );
+          const comparableMap = this.findObjFromArrByCoordinates(
+            this.game.map,
+            enemyEntity.tileX + 1,
+            enemyEntity.tileY,
+          );
+          comparableEntities.index == -1 && comparableMap.tileType != 'tileW'
+            ? this.move(enemyEntity, 1, 0)
+            : console.log('going right must attack');
+        } else {
+          // Y
+          // npc going left
+          const comparableEntities = this.findObjFromArrByCoordinates(
+            this.game.entities,
+            enemyEntity.tileX - 1,
+            enemyEntity.tileY,
+          );
+          const comparableMap = this.findObjFromArrByCoordinates(
+            this.game.map,
+            enemyEntity.tileX - 1,
+            enemyEntity.tileY,
+          );
+          comparableEntities.index == -1 && comparableMap.tileType != 'tileW'
+            ? this.move(enemyEntity, -1, 0)
+            : console.log('going left must attack');
+        }
+      } else {
+        if (distanceY > 0) {
+          // npc goes bottom
+          const comparableEntities = this.findObjFromArrByCoordinates(
+            this.game.entities,
+            enemyEntity.tileX,
+            enemyEntity.tileY + 1,
+          );
+          const comparableMap = this.findObjFromArrByCoordinates(
+            this.game.map,
+            enemyEntity.tileX,
+            enemyEntity.tileY + 1,
+          );
+          comparableEntities.index == -1 && comparableMap.tileType !== 'tileW'
+            ? this.move(enemyEntity, 0, 1)
+            : console.log('going bottom must attack');
+        } else {
+          // npc goes upper
+          const comparableEntities = this.findObjFromArrByCoordinates(
+            this.game.entities,
+            enemyEntity.tileX,
+            enemyEntity.tileY - 1,
+          );
+          const comparableMap = this.findObjFromArrByCoordinates(
+            this.game.map,
+            enemyEntity.tileX,
+            enemyEntity.tileY - 1,
+          );
+          comparableEntities.index == -1 && comparableMap.tileType != 'tileW'
+            ? this.move(enemyEntity, 0, -1)
+            : console.log('going top must attack');
+        }
+      }
+
+      setTimeout(() => {
+        recursy();
+      }, 500);
+    };
+
+    recursy();
+  }
+
+  move(entity, distaceX, distanceY) {
+    const currentEntity = {
+      ...entity,
+      tileX: entity.tileX + distaceX,
+      tileY: entity.tileY + distanceY,
+    };
+    const entityElem = document.getElementById(`${currentEntity.tileType}${currentEntity.id}`);
+
+    entityElem.className = currentEntity.tileType;
+    entityElem.style = `left: ${currentEntity.tileX * 50}px; top: ${currentEntity.tileY * 50}px`;
+
+    this.game.entities[currentEntity.index] = currentEntity;
   }
 }
